11bbdad89acf10cb39caf18a463d02ba63b50aa7
Or Bachar <or@localcoin.com>
2018-07-17 16:22:59 +0300

    working on exchanges

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ import * as utils from './utils';
 let _graphData = null;
 
 $(() => {
+    loadExchanges();
     $('#neo4j-address-search').submit(e => {
         e.preventDefault();
         const value = $('#neo4j-address-search').find('input').val();
@@ -17,6 +18,23 @@ $(() => {
         selector: '[data-toggle="tooltip"]'
     });
 });
+function loadExchanges() {
+    $.ajax({
+        url: 'https://spreadsheets.google.com/feeds/list/1LdmxfHHcPO3YZuHKEuTxNPdoqP2BkK3Rqyg76rHEqH8/od6/public/values?alt=json-in-script&callback=loadExchanges',
+
+        // The name of the callback parameter, as specified by the YQL service
+        jsonp: 'callback',
+
+        // Tell jQuery we're expecting JSONP
+        dataType: 'jsonp',
+
+        // Work with the response
+        success(response) {
+            debugger;
+            console.log(response); // server response
+        }
+    });
+}
 
 function isAddressValid(address) {
     if (!address || address.length !== 42) return false;
@@ -32,7 +50,7 @@ function loadTxsByAddress(address) {
     neo4jApi.loadTxsByAddress(address).then((txs => {
         $graphEl.html('');
         _graphData = neo4jApi.convertTxsToGraphData(txs);
-        renderGraph();
+        renderGraph(address);
     }));
 }
 
@@ -42,11 +60,13 @@ function mergeGraphData(newGraphData) {
     utils.mergeLists(_graphData.links, newGraphData.links, (a, b) => b.hash === a.hash);
 }
 
-function renderGraph() {
+function renderGraph(address) {
+    const initialAddress = address;
     const width = 1200;
     const height = 500;
 
     const circleRadius = 30;
+    // _graphData.nodes[0].fx = circleRadius;
 
     let svg = window.d3.select('#neo4j-graph')
         .append('svg')
@@ -57,7 +77,7 @@ function renderGraph() {
         }))
         .append('g');
 
-    // // link arrow
+    // link arrow
     // svg
     //     .append('svg:defs')
     //     .append('svg:marker')
@@ -71,6 +91,22 @@ function renderGraph() {
     //     .append('path')
     //     .attr('d', 'M0,-5L10,0L0,5')
     //     .style('fill', 'black');
+    svg
+        .append('svg:defs')
+        .append('svg:pattern')
+        .attr('id', 'binance-image')
+        .attr('x', '0%')
+        .attr('y', '0%')
+        .attr('height', '100%')
+        .attr('width', '100%')
+        .attr('viewBox', '0 0 50 50')
+        // .attr('patternUnits', 'userSpaceOnUse')
+        .append('image')
+        .attr('x', '0%')
+        .attr('y', '0%')
+        .attr('height', 50)
+        .attr('width', 50)
+        .attr('xlink:href', 'https://cdn.freebiesupply.com/logos/large/2x/binance-coin-logo-png-transparent.png');
 
 
     let linkElements;
@@ -86,29 +122,33 @@ function renderGraph() {
     const linkForce = d3
         .forceLink()
         .id(link => link.id)
-        // .strength(link => link.strength);
+        .strength(link => link.strength)
         .strength(0.1);
 
     const simulation = d3
         .forceSimulation()
         .force('link', linkForce)
-        .force('charge', d3.forceManyBody().strength(-320))
-        .force('center', d3.forceCenter(width / 2, height / 2));
+        .force('charge', d3.forceManyBody().strength(-200))
+        .force('center', d3.forceCenter(width / 2, height / 2))
+        .force('collision', d3.forceCollide().radius(d => d.radius));
 
-    const dragDrop = d3.drag().on('start', node => {
-        node.fx = node.x;
-        node.fy = node.y;
-    }).on('drag', node => {
-        simulation.alphaTarget(0.7).restart();
-        node.fx = d3.event.x;
-        node.fy = d3.event.y;
-    }).on('end', node => {
-        if (!d3.event.active)
-            simulation.alphaTarget(0);
+    const dragDrop = d3.drag()
+        .on('start', node => {
+            node.fx = node.x;
+            node.fy = node.y;
+        })
+        .on('drag', d => {
+            simulation.alphaTarget(0.7).restart();
+            d.fx = d3.event.x;
+            d.fy = d3.event.y;
+        })
+        .on('end', node => {
+            if (!d3.event.active)
+                simulation.alphaTarget(0);
 
-        node.fx = null;
-        node.fy = null;
-    });
+            // node.fx = null;
+            // node.fy = null;
+        });
 
     function onNodeClick(d) {
         neo4jApi.loadTxsByAddress(d.id).then(txs => {
@@ -116,6 +156,8 @@ function renderGraph() {
             mergeGraphData(graphData);
             updateSimulation();
         });
+        if (d.id == '0xcb270ac07fbdb44c0ff886ac994cf2ea54b0130d')
+            $('#modalExchangeInfo').modal('toggle');
     }
 
     function onNodeMouseOver() {
@@ -123,17 +165,17 @@ function renderGraph() {
         d3.select(this)
             .transition() // apply a transition
             .duration(300)
-            .attr('r', circleRadius + 2)
-            .attr('stroke-width', 6);
+            .attr('r', circleRadius + 4);
+        // .attr('stroke-width', 6);
     }
 
     function onNodeMouseOut() {
         // Use D3 to select element, change color and size
         d3.select(this)
             .transition()
-            .duration(150)
-            .attr('r', circleRadius)
-            .attr('stroke-width', 4);
+            .duration(300)
+            .attr('r', circleRadius);
+        // .attr('stroke-width', 4);
     }
 
     function getLinkStrokeWidth(d) {
@@ -145,8 +187,14 @@ function renderGraph() {
     }
 
     function getNodeFill(d) {
-        // if (d.id == '0xcb270ac07fbdb44c0ff886ac994cf2ea54b0130d') return 'url(#binance-image)';
-        return '#343a45';
+        if (d.id == '0xcb270ac07fbdb44c0ff886ac994cf2ea54b0130d') return 'url(#binance-image)';
+        if (d.id == initialAddress) return '#0A2A3B';
+        return '#2298D6';
+    }
+
+    function getNodeText(d) {
+        if (d.id == '0xcb270ac07fbdb44c0ff886ac994cf2ea54b0130d') return null;
+        return d.id.substr(0, 8) + '..';
     }
 
     function updateGraph() {
@@ -157,7 +205,7 @@ function renderGraph() {
             .enter()
             .append('path')
             .attr('stroke-width', getLinkStrokeWidth)
-            .attr('stroke', 'rgba(50, 50, 50, 0.2)')
+            .attr('stroke', '#B5E0F7')
             .attr('fill', 'none');
         // .attr('marker-end', 'url(#arrow)'); // append arraow to the line
 
@@ -172,8 +220,8 @@ function renderGraph() {
             .append('circle')
             .attr('r', circleRadius) // TODO move to param - to sync with arrow
             .attr('fill', getNodeFill)
-            .attr('stroke', '#dbdbdb')
-            .attr('stroke-width', 4)
+            // .attr('stroke', '#dbdbdb')
+            // .attr('stroke-width', 4)
             .call(dragDrop)
             .on('click', onNodeClick)
             .on('mouseover', onNodeMouseOver)
@@ -188,11 +236,12 @@ function renderGraph() {
         const textEnter = textElements
             .enter()
             .append('text')
-            .text(node => node.id.substr(0, 8) + '..')
+            .text(getNodeText)
             .attr('font-size', 9)
             .attr('fill', '#fff')
             .attr('dx', -21)
-            .attr('dy', 2);
+            .attr('dy', 2)
+            .style('pointer-events', 'none');
 
         textElements = textEnter.merge(textElements);
     }
