e7739b14e7d524b0df3e5c30febf5583a5d4e0cc
Or Bachar <or@localcoin.com>
2018-07-22 17:33:34 +0300

    add distance util

--- src/js/utils.js
+++ src/js/utils.js
@@ -2,3 +2,10 @@ export function mergeLists(aArr = [], bArr = [], comparer) {
     const difference = bArr.filter(b => !aArr.some(a => comparer(a, b)));
     aArr.push(...difference);
 }
+
+export function getDistanceBetweenPoints(x1, y1, x2, y2) {
+    const a = x1 - x2;
+    const b = y1 - y2;
+
+    return Math.sqrt(a * a + b * b);
+}
